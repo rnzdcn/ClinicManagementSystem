b21cf042aa513f479a303d963512092b798ac65d
Jomz Custodio <jomaricustodio1997@gmail.com>
2021-12-03 09:19:39 +0800

    button
    set visible

--- src/Clinic/Inventory.java
+++ src/Clinic/Inventory.java
@@ -254,6 +254,7 @@ public class Inventory extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         new addnewMedicine().setVisible(true);
+        new Main().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
